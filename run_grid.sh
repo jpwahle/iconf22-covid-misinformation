080600e00a5c70e29fccde3dacc5a8a31eb09ba3
Jan Philip Wahle <jan.philip.wahle@gmail.com>
2021-08-11 14:31:19 +0200

    Run 2. Larger batch size and 2 epochs seem sufficient.

--- run_grid.sh
+++ run_grid.sh
@@ -1,6 +1,5 @@
 grid run \
   --instance_type g4dn.xlarge finetuning.py \
   --dataset_path grid:covid-fakenews:2 \
-  --model_name_or_path allenai/longformer-base-4096 \
+  --model_name_or_path "['allenai/scibert_scivocab_uncased', 'emilyalsentzer/Bio_ClinicalBERT', 'facebook/bart-base', 'deepset/covid_bert_base', 'vinai/bertweet-base', 'digitalepidemiologylab/covid-twitter-bert-v2', 'manueltonneau/clinicalcovid-bert-base-cased', 'manueltonneau/biocovid-bert-large-cased', 'roberta-base', 'bert-base-uncased', 'microsoft/deberta-base']"  \
   --dataset_name "['cmu', 'par', 'rec', 'fn19', 'coaid']"
-  # --model_name_or_path "['allenai/scibert_scivocab_uncased', 'emilyalsentzer/Bio_ClinicalBERT', 'facebook/bart-base', 'deepset/covid_bert_base', 'vinai/bertweet-base', 'digitalepidemiologylab/covid-twitter-bert-v2', 'manueltonneau/clinicalcovid-bert-base-cased', 'manueltonneau/biocovid-bert-large-cased', 'allenai/longformer-base-4096']" \
